7aac3413f08a5255081bac0373be8688a2be48aa
Lucas Santiago <lucahh@gmail.com>
2024-10-09 19:14:07 -0300

    :wrench: feat Nome projeto
    alteração de nomes nos documentos iniciais.

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -4,8 +4,8 @@ export const swaggerConfig: swaggerJSDoc.OAS3Options = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-      title: 'Orion API',
-      description: 'Documentação da API do projeto Orion.',
+      title: 'Reforça o Bem API',
+      description: 'Documentação da API do projeto Reforça o Bem.',
       version: '1.0.0'
     },
     host: 'localhost:4444',
