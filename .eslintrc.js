ec521eed09152396ef32ce7bec3b53450be12a23
Lucas Santiago <lucahh@gmail.com>
2024-10-15 14:03:12 -0300

    Removendo prettier e padronizando a formatação e identação

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,10 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended'  
   ],
   root: true,
   env: {
@@ -20,6 +20,13 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
-    'prettier/prettier': ['error', { trailingComma: 'none' }]
+    'prettier/prettier': ['error', {
+      trailingComma: 'none',   
+      tabWidth: 4,             
+      useTabs: false,          
+      semi: true,              
+      singleQuote: true,       
+      endOfLine: 'lf'          
+    }]
   }
 };
