66fafa6fee2362b4f6200a2350f6aafeefb7e47f
isaacbatista26 <isaacbatista2266@gmail.com>
2024-10-16 20:00:29 -0300

    feat: Add new routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,34 @@ import { Router } from 'express';
 import { HomeController } from './controller/HomeController';
 import { TutorController } from './controller/TutorController';
 import { TutorValidator } from './validator/TutorValidator';
+import { StudentController } from './controller/StudentController';
+import { EducationLevelController } from './controller/EducationLevelController';
 
 const router = Router();
 
+// Home route
 router.get('/', new HomeController().hello);
 
+// Tutor routes
 router.post(
   '/api/register/tutor',
   TutorValidator.createTutor(),
   new TutorController().create
 );
+
+router.get('/api/get/tutor', new TutorController().getAll);
+
+// Students routes
+router.get('/api/get/student', new StudentController().getAll);
+
+router.post('/api/register/student', new StudentController().create);
+
+// Education Level routes
+router.get(
+  '/api/register/educationlevel',
+  new EducationLevelController().create
+);
+
+router.get('/api/get/educationlevel', new EducationLevelController().getAll);
 
 export default router;
