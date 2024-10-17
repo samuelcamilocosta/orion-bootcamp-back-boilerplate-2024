2fbf166f19ee3d496100848d295e3ccdca6aa3d8
isaacbatista26 <isaacbatista2266@gmail.com>
2024-10-16 21:25:38 -0300

    chore: solve lint problems

--- src/entity/Tutor.ts
+++ src/entity/Tutor.ts
@@ -5,35 +5,34 @@ import { User } from './User';
 
 @Entity()
 export class Tutor extends User {
+  @Column({ type: 'varchar', length: 11, unique: true, nullable: false })
+  cpf: string;
 
-    @Column({ type: 'varchar', length: 11, unique: true,  nullable: false })
-    cpf: string;
+  @ManyToMany(() => Subject, { cascade: true })
+  @JoinTable({
+    name: 'tutor_subjects_subject',
+    joinColumn: {
+      name: 'tutorId',
+      referencedColumnName: 'id'
+    },
+    inverseJoinColumn: {
+      name: 'subjectId',
+      referencedColumnName: 'subjectId'
+    }
+  })
+  subjects: Subject[];
 
-    @ManyToMany(() => Subject, { cascade: true})
-    @JoinTable({
-        name: 'tutor_subjects_subject',
-        joinColumn: {
-            name: 'tutorId',
-            referencedColumnName: 'id',
-        },
-        inverseJoinColumn: {
-            name: 'subjectId',
-            referencedColumnName: 'subjectId',
-        }
-    })
-    subjects: Subject[];
-
-    @ManyToMany(() => EducationLevel, { cascade: true})
-    @JoinTable({
-        name: 'tutor_education_levels',
-        joinColumn: {
-            name: 'tutorId',
-            referencedColumnName: 'id',
-        },
-        inverseJoinColumn: {
-            name: 'educationLevelId',
-            referencedColumnName: 'educationId',
-        }
-    })
-    educationLevels: EducationLevel[];
+  @ManyToMany(() => EducationLevel, { cascade: true })
+  @JoinTable({
+    name: 'tutor_education_levels',
+    joinColumn: {
+      name: 'tutorId',
+      referencedColumnName: 'id'
+    },
+    inverseJoinColumn: {
+      name: 'educationLevelId',
+      referencedColumnName: 'educationId'
+    }
+  })
+  educationLevels: EducationLevel[];
 }
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -2,24 +2,24 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
-    username: string;
+  @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
+  username: string;
 
-    @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
-    email: string;
+  @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
+  email: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: false })
-    password: string;
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  password: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: false })
-    salt: string;
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  salt: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: false })
-    fullName: string;
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  fullName: string;
 
-    @Column({ type: 'date', nullable: false })
-    birthDate: Date;
-} 
+  @Column({ type: 'date', nullable: false })
+  birthDate: Date;
+}
