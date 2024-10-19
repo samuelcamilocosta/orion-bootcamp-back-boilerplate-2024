a110f932c78c7f4e55085e837f65553a23317c83
Rafael Cruz <rcas2009@outlook.com>
2024-10-19 00:45:10 -0300

    fix: implement cors error fix in other routes

--- src/routes.ts
+++ src/routes.ts
@@ -17,17 +17,19 @@ router.get('/', new HomeController().hello);
 // Tutor routes
 router.post(
   '/api/register/tutor',
+  cors(),
   TutorValidator.createTutor(),
   new TutorController().create
 );
 
-router.get('/api/get/tutor', new TutorController().getAll);
+router.get('/api/get/tutor', cors(), new TutorController().getAll);
 
 // Students routes
-router.get('/api/get/student', new StudentController().getAll);
+router.get('/api/get/student', cors(), new StudentController().getAll);
 
 router.post(
   '/api/register/student',
+  cors(),
   StudentValidator.createStudent(),
   new StudentController().create
 );
@@ -35,10 +37,15 @@ router.post(
 // Education Level routes
 router.post(
   '/api/register/educationlevel',
+  cors(),
   new EducationLevelController().create
 );
 
-router.get('/api/get/educationlevel', new EducationLevelController().getAll);
+router.get(
+  '/api/get/educationlevel',
+  cors(),
+  new EducationLevelController().getAll
+);
 
 // Login route
 router.post(
