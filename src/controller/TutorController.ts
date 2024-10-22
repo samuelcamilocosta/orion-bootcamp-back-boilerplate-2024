433d9fee8a70ca1f10bdc3cf36900485b297378e
Rafael Cruz <rcas2009@outlook.com>
2024-10-21 21:42:55 -0300

    chore: remove console.log

--- src/controller/TutorController.ts
+++ src/controller/TutorController.ts
@@ -171,13 +171,10 @@ export class TutorController {
   }
 
   async getAll(req: Request, res: Response) {
-    console.log('Rota getAll de tutores acessada.');
     try {
       const tutor = await MysqlDataSource.getRepository(Tutor).find();
-      console.log('Tutores encontrados:', tutor);
       return res.status(200).json(tutor);
     } catch (error) {
-      console.error('Erro ao buscar tutores:', error);
       return res.status(500).json({ message: 'Erro interno do servidor.' });
     }
   }
