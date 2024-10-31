7612310cb25a6f7e4c6e57c128964ab763f6466a
Rafael Cruz <rcas2009@outlook.com>
2024-10-31 20:24:00 -0300

    feat: create insert subjects migration

---
+++ src/migrations/1730416827233-InsertSubjects.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InsertSubjects1643348574000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const tableExists = await queryRunner.hasTable('education_level');
+    if (tableExists) {
+      await queryRunner.query(`
+        INSERT INTO subject (subjectName) VALUES 
+        ('Biologia'),
+        ('Sociologia'),
+        ('Gramática'),
+        ('História'),
+        ('Filosofia'),
+        ('Literatura'),
+        ('Geografia'),
+        ('Física'),
+        ('Redação'),
+        ('Inglês'),
+        ('Química'),
+        ('Matemática');
+        `);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      DELETE FROM subject WHERE subjectName IN ('Biologia', 'Sociologia', 'Gramática', 'História', 'Filosofia', 'Literatura', 'Geografia', 'Física', 'Redação', 'Inglês', 'Química', 'Matemática');
+    `);
+  }
+}
