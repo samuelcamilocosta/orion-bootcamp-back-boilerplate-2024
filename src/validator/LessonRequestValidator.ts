af05e45b9c9415d8a351770958b68e2ff84ba17d
Rafael Cruz <rcas2009@outlook.com>
2024-11-04 14:25:37 -0300

    fix: correct date Reges and add real date validation

--- src/validator/LessonRequestValidator.ts
+++ src/validator/LessonRequestValidator.ts
@@ -31,12 +31,23 @@ export class LessonRequestValidator {
           'Datas preferidas são obrigatórias. Mínimo de 1 e máximo de 3.'
         )
         .custom((value) => {
-          const dateRegex = /^\d{2}\/\d{2}\/\d{4} às \d{2}:\d{2}$/;
+          const dateRegex =
+            /^(0[1-9]|1[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{4} às \d{2}:\d{2}$/;
           value.forEach((date) => {
             if (typeof date !== 'string' || !dateRegex.test(date)) {
               throw new Error(
                 'Data inválida. O formato correto é dd/MM/yyyy às HH:mm.'
               );
+            }
+            const [day, month, yearTime] = date.split('/');
+            const [year] = yearTime.split(' às ');
+            const dateObject = new Date(`${year}-${month}-${day}`);
+            if (
+              dateObject.getFullYear() !== parseInt(year) ||
+              dateObject.getMonth() + 1 !== parseInt(month) ||
+              dateObject.getDate() !== parseInt(day)
+            ) {
+              throw new Error('Data inválida. Verifique se a data existe.');
             }
           });
           return true;
