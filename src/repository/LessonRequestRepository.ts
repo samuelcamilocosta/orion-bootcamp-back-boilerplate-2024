de1c3e5a73c8d4a6dc5c5d4e104818d884b121d0
Rafael Cruz <rcas2009@outlook.com>
2024-11-04 13:48:52 -0300

    feat: create lesson request repository

---
+++ src/repository/LessonRequestRepository.ts
@@ -0,0 +1,20 @@
+import { MysqlDataSource } from "config/database";
+import { LessonRequest } from "../entity/LessonRequest";
+
+export class LessonRequestRepository {
+    static async createLessonRequest(lessonRequest: LessonRequest): Promise<LessonRequest> {
+        return await MysqlDataSource.getRepository(LessonRequest).save(lessonRequest);
+    }
+
+    static async findByPreferredDate(preferredDate: string, studentId: number): Promise<LessonRequest | null> {
+        return await MysqlDataSource.getRepository(LessonRequest).findOne({
+            where: { preferredDates: preferredDate, student: { id: studentId } }
+        });
+    }
+
+    static async getAllLessonRequests(): Promise<LessonRequest[]> {
+        return await MysqlDataSource.getRepository(LessonRequest).find({
+            relations: ['subject', 'student']
+        });
+    }
+}
