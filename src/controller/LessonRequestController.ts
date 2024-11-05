da053c1de89f5d85d69a1aede91e62670a1c5e37
Rafael Cruz <rcas2009@outlook.com>
2024-11-05 10:48:05 -0300

    chore: move swagger doc

--- src/controller/LessonRequestController.ts
+++ src/controller/LessonRequestController.ts
@@ -6,140 +6,140 @@ import { Subject } from '../entity/Subject';
 import { Student } from '../entity/Student';
 import { LessonRequestRepository } from '../repository/LessonRequestRepository';
 
-/**
- * @swagger
- * /api/register/lessonrequest:
- *   post:
- *     summary: Create a new lesson request
- *     tags: [LessonRequest]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               reason:
- *                 type: array
- *                 items:
- *                   type: string
- *                 description: Reasons for the lesson
- *                 example: ["reforço", "prova ou trabalho"]
- *               preferredDates:
- *                 type: array
- *                 items:
- *                   type: string
- *                 description: Preferred dates for the lesson
- *                 example: ["22/12/2024 às 10:00"]
- *               subjectId:
- *                 type: integer
- *                 description: ID of the subject
- *                 example: 1
- *               additionalInfo:
- *                 type: string
- *                 description: Additional information for the lesson request
- *                 example: "Informações adicionais"
- *               studentId:
- *                 type: integer
- *                 description: ID of the student
- *                 example: 1
- *     responses:
- *       '201':
- *         description: Lesson request created successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "Seu pedido de aula foi enviado com sucesso!"
- *                 lessonRequest:
- *                   type: object
- *                   properties:
- *                     reason:
- *                       type: array
- *                       items:
- *                         type: string
- *                     preferredDates:
- *                       type: array
- *                       items:
- *                         type: string
- *                     additionalInfo:
- *                       type: string
- *                     status:
- *                       type: string
- *                     subject:
- *                       type: object
- *                       properties:
- *                         subjectId:
- *                           type: integer
- *                         subjectName:
- *                           type: string
- *                     student:
- *                       type: object
- *                       properties:
- *                         id:
- *                           type: integer
- *                         username:
- *                           type: string
- *                         email:
- *                           type: string
- *                         password:
- *                           type: string
- *                         salt:
- *                           type: string
- *                         fullName:
- *                           type: string
- *                         birthDate:
- *                           type: string
- *                           format: date
- *                     ClassId:
- *                       type: integer
- *       '400':
- *         description: Bad request, validation errors
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: object
- *                   properties:
- *                     errors:
- *                       type: array
- *                       items:
- *                         type: object
- *                         properties:
- *                           type:
- *                             type: string
- *                             example: "field"
- *                           value:
- *                             type: array
- *                             items:
- *                               type: string
- *                             example: ["wrong reason"]
- *                           msg:
- *                             type: string
- *                             example: "Motivo da aula inválido. Deve conter ao menos um desses: reforço, prova ou trabalho, correção de exercício, outro."
- *                           path:
- *                             type: string
- *                             example: "reason"
- *                           location:
- *                             type: string
- *                             example: "body"
- *       '500':
- *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "Erro interno do servidor."
- */
 export class LessonRequestController {
+  /**
+   * @swagger
+   * /api/register/lessonrequest:
+   *   post:
+   *     summary: Create a new lesson request
+   *     tags: [LessonRequest]
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               reason:
+   *                 type: array
+   *                 items:
+   *                   type: string
+   *                 description: Reasons for the lesson
+   *                 example: ["reforço", "prova ou trabalho"]
+   *               preferredDates:
+   *                 type: array
+   *                 items:
+   *                   type: string
+   *                 description: Preferred dates for the lesson
+   *                 example: ["22/12/2024 às 10:00"]
+   *               subjectId:
+   *                 type: integer
+   *                 description: ID of the subject
+   *                 example: 1
+   *               additionalInfo:
+   *                 type: string
+   *                 description: Additional information for the lesson request
+   *                 example: "Informações adicionais"
+   *               studentId:
+   *                 type: integer
+   *                 description: ID of the student
+   *                 example: 1
+   *     responses:
+   *       '201':
+   *         description: Lesson request created successfully
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                   example: "Seu pedido de aula foi enviado com sucesso!"
+   *                 lessonRequest:
+   *                   type: object
+   *                   properties:
+   *                     reason:
+   *                       type: array
+   *                       items:
+   *                         type: string
+   *                     preferredDates:
+   *                       type: array
+   *                       items:
+   *                         type: string
+   *                     additionalInfo:
+   *                       type: string
+   *                     status:
+   *                       type: string
+   *                     subject:
+   *                       type: object
+   *                       properties:
+   *                         subjectId:
+   *                           type: integer
+   *                         subjectName:
+   *                           type: string
+   *                     student:
+   *                       type: object
+   *                       properties:
+   *                         id:
+   *                           type: integer
+   *                         username:
+   *                           type: string
+   *                         email:
+   *                           type: string
+   *                         password:
+   *                           type: string
+   *                         salt:
+   *                           type: string
+   *                         fullName:
+   *                           type: string
+   *                         birthDate:
+   *                           type: string
+   *                           format: date
+   *                     ClassId:
+   *                       type: integer
+   *       '400':
+   *         description: Bad request, validation errors
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: object
+   *                   properties:
+   *                     errors:
+   *                       type: array
+   *                       items:
+   *                         type: object
+   *                         properties:
+   *                           type:
+   *                             type: string
+   *                             example: "field"
+   *                           value:
+   *                             type: array
+   *                             items:
+   *                               type: string
+   *                             example: ["wrong reason"]
+   *                           msg:
+   *                             type: string
+   *                             example: "Motivo da aula inválido. Deve conter ao menos um desses: reforço, prova ou trabalho, correção de exercício, outro."
+   *                           path:
+   *                             type: string
+   *                             example: "reason"
+   *                           location:
+   *                             type: string
+   *                             example: "body"
+   *       '500':
+   *         description: Internal server error
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                   example: "Erro interno do servidor."
+   */
   async create(req: Request, res: Response) {
     const { reason, preferredDates, subjectId, additionalInfo, studentId } =
       req.body;
