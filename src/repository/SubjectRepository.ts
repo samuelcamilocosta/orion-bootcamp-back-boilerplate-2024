b07f6c823628a132e543aa4918e50b9db978441f
Rafael Cruz <rcas2009@outlook.com>
2024-11-11 21:42:19 -0300

    feat: add find subject by ids function

--- src/repository/SubjectRepository.ts
+++ src/repository/SubjectRepository.ts
@@ -1,5 +1,6 @@
 import { MysqlDataSource } from '../config/database';
 import { Subject } from '../entity/Subject';
+import { In } from 'typeorm';
 
 export class SubjectRepository {
   static async saveSubject(subject: Subject): Promise<Subject> {
@@ -10,6 +11,11 @@ export class SubjectRepository {
   static async findSubjectById(subjectId: number): Promise<Subject | null> {
     const repository = MysqlDataSource.getRepository(Subject);
     return await repository.findOne({ where: { subjectId } });
+  }
+
+  static async findSubjectByIds(subjectIds: number[]): Promise<Subject[]> {
+    const repository = MysqlDataSource.getRepository(Subject);
+    return await repository.find({ where: { subjectId: In(subjectIds) } });
   }
 
   static async findAllSubjects() {
